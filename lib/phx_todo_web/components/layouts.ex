288569e871ad38cd3f2901176a495f8477580454
Alex Thomson <alexthomsonnz@gmail.com>
2023-11-18 12:56:50 +1300

    Init phoenix todo app

---
+++ lib/phx_todo_web/components/layouts.ex
@@ -0,0 +1,5 @@
+defmodule PhxTodoWeb.Layouts do
+  use PhxTodoWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ lib/phx_todo_web/controllers/page_html.ex
@@ -0,0 +1,5 @@
+defmodule PhxTodoWeb.PageHTML do
+  use PhxTodoWeb, :html
+
+  embed_templates "page_html/*"
+end
